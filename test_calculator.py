42e5bc87e1d0cbe860255dcfe85ea1c3f901dbd7
Casey Nord <nordcw@gmail.com>
2021-06-02 17:10:50 -0700

    adding basic unit tests for calculator app

---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+"""
+CS362 - Unit Tests for Calculator App Continuous Integration
+"""
+
+import calculator
+
+
+class TestCalculatorApp:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
+        
